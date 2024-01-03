1ca912b453338076b4416579e377a50271ccd0e3
Justin Sharma <justin.elite@gmail.com>
2024-01-03 15:13:04 -0700

    chore: simplify version flag and call built app in action

--- src/odyssey_example_cli/cli.py
+++ src/odyssey_example_cli/cli.py
@@ -4,7 +4,7 @@ from odyssey_example_cli.__about__ import __version__
 
 
 @click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=True)
-@click.version_option(version=__version__, prog_name='new cli')
+@click.version_option(version=__version__, message='%(version)s')
 @click.pass_context
 def cli(ctx: click.Context):
     click.echo('Hello world!')
